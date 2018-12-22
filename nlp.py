136d7267718f955f5e4666842e532decd92695fd
Md Saroar Jahan <43512597+saroarjahan@users.noreply.github.com>
2018-12-22 16:32:33 +0200

    category added

--- nlp.py
+++ nlp.py
@@ -8,5 +8,15 @@ from sklearn.metrics import roc_auc_score
 
 from empath import Empath #for accuracy
 lexicon = Empath()
-x=lexicon.analyze("he hit hit , kill  dead   the other person. ", categories=["violence"], normalize=True)
+x=lexicon.analyze("he hit hit , kill  dead   the other person. ",
+categories=["violence",
+            "aggresion",
+            "crime",
+            "horror",
+            "kill",
+            "neglect",
+            'dominant_personality',
+            'fight',
+            'terrorism',
+            'negative_emotion'], normalize=True)
 print(x)
