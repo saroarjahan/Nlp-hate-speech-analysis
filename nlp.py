e6a1ef994999a5f30e56a8e6fceafc0fab8a78aa
Md Saroar Jahan <43512597+saroarjahan@users.noreply.github.com>
2018-12-22 16:02:17 +0200

    update empath

--- nlp.py
+++ nlp.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import urllib
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
 
+from empath import Empath #for accuracy
+lexicon = Empath()
+x=lexicon.analyze("he hit hit , kill  dead   the other person. ", categories=["violence"], normalize=True)
+print(x)
